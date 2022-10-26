91fada9a37590d290b7a32c3d4a960efc3481a78
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-26 20:38:29 +0530

    Abilty to find word

--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@ namespace HashTableAndTree
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Hash Table Program");
+            //Console.WriteLine("Welcome to Hash Table Program");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0","To");
+            hash.Add("1"," be");
+            hash.Add("2","or");
+            hash.Add("3","not");
+            hash.Add("4","to");
+            hash.Add("5","be");
+
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index Value:  " +hash5);
+
            
         }
     }
