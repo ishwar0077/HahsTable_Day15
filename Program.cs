96d62e2e365156e34c5bb345116a93833060c6a9
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-26 20:35:51 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace HashTableAndTree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table Program");
+           
+        }
+    }
+}
